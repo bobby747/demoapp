58353b7060542ebe05914dacc831ab5f4bf94de3
RAJESH JUMBU <raj@RAJESHs-Mac-mini.local>
2022-09-07 15:34:33 -0400

    added contact route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'main/home'
+  #get 'main/home'
   get 'main/about'
+  get 'main/contact'
+  root 'main#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
